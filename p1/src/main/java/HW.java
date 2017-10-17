1da574b4b3b68da855e01cc06b13141ef18c43ea
Rastamaniac <32819211+Rastamaniac@users.noreply.github.com>
2017-10-17 22:48:43 +0300

    Сделали красиво функции

--- p1/src/main/java/HW.java
+++ p1/src/main/java/HW.java
@@ -1,12 +1,41 @@
-public class HW{
+public class HW {
 
 
-public static void main (String[] args){
+    public static void main(String[] args) {
+        qwe("Братишка ");
+        qwe ("сучечки ");
+
+        int s = 5;
+
+        System.out.println("плщдьквдрт с стороной s = " + s + "="+ area(s) );
+
+        int a = 14;
+        int b = 5;
+
+        System.out.println("Площадь прямоугольника с сторонами a и b "+ " = " + area(a,b) );
+
+    }
 
 
-System.out.println("Fuck with your mom1111");
+
+    public static void qwe (String text) {
+
+        int q = 12;
+
+        int w = 12;
+
+        System.out.println("Hello, " + text + q * w);
+    }
+        public static int area (int s){
+
+            return s*s;
+}
+                public static int area (int a, int b){
+                    return a*b;
+                }
 
 
-  }
+
+
 
 }
