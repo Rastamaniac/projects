229b283dfe6931dacdb472c125dde54dfe7b19bb
Rastamaniac <32819211+Rastamaniac@users.noreply.github.com>
2017-10-29 19:43:25 +0300

    Вычесление точек на плоскости

--- p1/src/main/java/HW.java
+++ p1/src/main/java/HW.java
@@ -1,48 +1,13 @@
-
-
 public class HW{
 
-    public static void main (String[] args) {
+    public static void main(String[] args){
 
-        голова(", дай денег" );
-        голова(", че как дила?");
-
-
-            Square s = new Square(7);
-
-            Rectangle r = new Rectangle(4,3);
-
-        System.out.println("Площадь квадртата со сторонами " + s.l + " = " + area(s));
+        Point P1 = new Point(2.0,2.0);
+        Point P2 = new Point(0.0,0.0);
 
 
-                System.out.println("Площадь прямоугольника с сторонами " + r.a +" и "+  + r.b + " = " + перимитр(r));
+        System.out.println(Point.Distance(P1,P2));
 
-        }
-
-        public static void голова (String text){
-
-            System.out.println( "Эй, голова"+ text);
-        }
-
-            public static double перимитр(Rectangle r){
-
-                return r.a*r.b;
-
-
-            }
-
-
-                    public static double area(Square s){
-                        return s.l*s.l;
-
-
-                    }
-
-
-
-
-
-
+    }
 
 }
-
